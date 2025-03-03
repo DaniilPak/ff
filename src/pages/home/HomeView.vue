ade9d46fd3304af209813b837905d3b92cf7277f
pak_ks650 <daniil.pak@kaspi.kz>
2025-03-03 16:01:39 +0500

    1

--- src/pages/home/HomeView.vue
+++ src/pages/home/HomeView.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <h1>Welcome to Vue home</h1>
-  </div>
+  <section class="hero is-primary">
+    <div class="hero-body">
+      <p class="title">Welcome to Vue home</p>
+      <button class="button is-light" @click="sayHello">Click Me</button>
+    </div>
+  </section>
 </template>
+
+<script setup lang="ts">
+const sayHello = () => alert('Hello from Bulma!')
+</script>
