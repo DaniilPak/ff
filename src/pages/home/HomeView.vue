1dd74c3830e5d81c2be5da8906d8f21cd2e280a0
DaniilPak <101089638+DaniilPak@users.noreply.github.com>
2025-04-04 00:08:49 +0600

    Update HomeView.vue

--- src/pages/home/HomeView.vue
+++ src/pages/home/HomeView.vue
@@ -1,25 +1,38 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
+import { message } from 'ant-design-vue'
+const [messageApi, contextHolder] = message.useMessage()
 
 const check = ref(0)
 const minus = ref(0)
 const percent = ref(0.1)
 const checked = ref(false)
+const cashback = ref(0)
 
 const pay = computed(() => {
   const discount = check.value * percent.value
   return checked.value ? check.value - discount - minus.value : check.value - discount
 })
+
+const handlePay = () => {
+  cashback.value = pay.value * 0.1
+  messageApi.info('Payment complete!')
+}
 </script>
 
 <template>
+  <context-holder />
   <section>
     <a-space direction="vertical">
       <a-input v-model:value="check" placeholder="check" />
       <a-input v-model:value="minus" placeholder="minus" />
-      Spend bonus {{ minus }}<a-switch v-model:checked="checked" /> discount: {{ percent }}%
+      spend bonus {{ minus }}<a-switch v-model:checked="checked" /> discount: {{ percent }}%
       <br />
       summary: {{ pay }}
+
+      <a-button @click="handlePay">Pay</a-button>
+      <div v-if="cashback > 0">You gained cashback! {{ cashback }}</div>
     </a-space>
   </section>
 </template>
+  
