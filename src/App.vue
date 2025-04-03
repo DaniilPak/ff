fe02ded07d8ee3ae8b13432035bcdc56a8034976
DaniilPak <101089638+DaniilPak@users.noreply.github.com>
2025-04-03 23:43:42 +0600

    1

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,6 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  Except router
-  <a-button type="primary">Click Me</a-button>
   <header>
     <nav>
       <RouterLink to="/">Home</RouterLink>
