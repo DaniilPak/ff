c3833a24fe0297d3aaff352309e2e420daa91d67
DaniilPak <101089638+DaniilPak@users.noreply.github.com>
2025-04-16 19:59:30 +0500

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,20 @@
+// import { fileURLToPath, URL } from 'node:url'
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import vueJsx from '@vitejs/plugin-vue-jsx'
+// import vueDevTools from 'vite-plugin-vue-devtools'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [vue(), vueJsx(), vueDevTools()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url)),
+//     },
+//   },
+// })
+
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -5,12 +22,24 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+import { execSync } from 'child_process'
+
+const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
+const allowed = Array.from({ length: 10 }, (_, i) => `rc${i + 1}`)
+
+if (!allowed.includes(branch)) {
+  throw new Error(`❌ Branch '${branch}' is not allowed for build`)
+}
+
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  base: `/${branch}/`,
+  build: {
+    outDir: `dist/${branch}`,
   },
 })
